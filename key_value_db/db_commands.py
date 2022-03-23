f34945a2bbfe1e7b42e0d1853a348a4f04b6193c
Adivardhan Maheshwari <48666834+adivardhan1000@users.noreply.github.com>
2022-03-23 13:08:46 +0530

    basic commands

--- key_value_db/db_commands.py
+++ key_value_db/db_commands.py
@@ -1,2 +1,29 @@
-def SET(key, value):
-    pass
+def SET(conn, key, value=0):
+    try:
+        return conn.set(key,value)
+    except:
+        return False
+
+def GET(conn, key, value=0):
+    try:
+        return conn.get(key)
+    except:
+        return False
+
+def DEL(conn, key, value=0):
+    try:
+        return conn.delete(key)
+    except:
+        return False
+
+def INCR(conn, key, value = 1):
+    try:
+        return conn.incr(key,value)
+    except:
+        return False
+
+def INCRBY(conn, key, value = 1):
+    try:
+        return conn.incr(key,value)
+    except:
+        return False
