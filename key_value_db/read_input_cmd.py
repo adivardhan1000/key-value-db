372117db5034c127704a9e974b1d382ef3c219e1
Adivardhan Maheshwari <48666834+adivardhan1000@users.noreply.github.com>
2022-03-23 14:51:12 +0530

    add parse string reusable utility

--- key_value_db/read_input_cmd.py
+++ key_value_db/read_input_cmd.py
@@ -1,12 +1,21 @@
-import db_commands
+from db_commands import commands
+
+def parse_string_call_db_commands(comm, userInput):
+    '''Reusable to call the db commands
+    Inputs: 
+        db_commands.commands() Object
+        user Input             String'''
+    userCommand = userCommand.strip().split()
+    return getattr(comm, userCommand[0])(userCommand)
+
 
 def processInputInCMD(conn):
     print("To Stop enter x")
     print("Enter commands to process below")
+    comm = commands(conn)
     while True:
         userCommand = input(">>")
         if userCommand.lower() == 'x':
             break
-        userCommand = userCommand.strip().split()
-        result = getattr(db_commands, userCommand[0])(conn, userCommand[1],userCommand[2])
-        print(result)
+        print(parse_string_call_db_commands(comm, userCommand))
+        
